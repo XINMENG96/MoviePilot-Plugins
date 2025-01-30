a5fa416b2dd72903b89f5222f4f5be85c0ad9d8b
XINMENG96 <9sterev6@gmail.com>
2025-01-31 00:41:55 +0800

    1

--- plugins/anistrm_mod/__init__.py
+++ plugins/anistrm_mod/__init__.py
@@ -60,7 +60,7 @@ class ANiStrm(_PluginBase):
     # 插件版本
     plugin_version = "2.4.3"
     # 插件作者
-    plugin_author = "honue, XINMENG96"
+    plugin_author = "XINMENG96"
     # 作者主页
     author_url = "https://github.com/XINMENG96"
     # 插件配置项ID前缀
